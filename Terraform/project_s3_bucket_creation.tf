45d7ed69ad97ca6a3232bfa0aa9df296a4883289
Jibin Thomas <jibints91@gmail.com>
2026-04-13 15:41:05 +1000

    Initial commit

---
+++ Terraform/project_s3_bucket_creation.tf
@@ -0,0 +1,54 @@
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#   }
+# }
+
+# provider "aws" {
+#   region = "ap-southeast-2"
+# }
+
+resource "aws_s3_bucket" "parent_bucket" {
+  bucket        = "my-finance-etl-project-bucket"
+  force_destroy = true
+}
+
+resource "aws_s3_object" "project_prefix_raw" {
+  bucket  = aws_s3_bucket.parent_bucket.id
+  key     = "raw/"
+  content = ""
+}
+
+resource "aws_s3_object" "project_prefix_processed" {
+  bucket  = aws_s3_bucket.parent_bucket.id
+  key     = "processed/"
+  content = ""
+}
+
+resource "aws_s3_object" "project_prefix_rejected" {
+  bucket  = aws_s3_bucket.parent_bucket.id
+  key     = "rejected/"
+  content = ""
+}
+
+resource "aws_s3_object" "project_prefix_glue_jobs" {
+  bucket  = aws_s3_bucket.parent_bucket.id
+  key     = "glue_jobs/"
+  content = ""
+}
+
+
+resource "aws_s3_object" "project_prefix_misc" {
+  bucket  = aws_s3_bucket.parent_bucket.id
+  key     = "misc/"
+  content = ""
+}
+
+resource "aws_s3_object" "project_prefix_logs" {
+  bucket  = aws_s3_bucket.parent_bucket.id
+  key     = "logs/"
+  content = ""
+}
